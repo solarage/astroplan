bb5458f0ac0bbeae04d8f8ddaca239cd90e91266
Alex <35805922+solarage@users.noreply.github.com>
2020-07-27 11:07:51 +0300

    Add utils lib

---
+++ src/utils/utils.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-param-reassign */
+function identity(value) {
+  return value !== undefined ? () => value : (val) => val;
+}
+
+function ensureFunction(fn, fallback) {
+  if (fn === '%local' || fn === '%l') fn = function () { return this; };
+
+  return typeof fn === 'function' ? fn : fallback(fn);
+}
+
+export function getter(name) {
+  const path = name ? name.split('.') : [];
+  const len = path.length;
+
+  return function (ctx) {
+    let idx = 0;
+
+    while (ctx && idx < len) ctx = ctx[path[idx++]];
+
+    return ctx;
+  };
+}
+
+export function firstOf(...conditions) {
+  const fns = [];
+
+  for (const condition of conditions) {
+    fns.push(ensureFunction(condition, identity));
+  }
+
+  return function (...args) {
+    for (const fn of fns) {
+      const result = fn.call(this, ...args);
+
+      if (result) return result;
+    }
+
+    return false;
+  };
+}
+
+export function eql(key, value) {
+  key = ensureFunction(key, getter);
+  value = ensureFunction(value, identity);
+
+  return function (context) {
+    return key.call(this, context) === value.call(this, context);
+  };
+}
